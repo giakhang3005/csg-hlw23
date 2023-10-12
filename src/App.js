af328744de6be3f09d89e809ea1b9f8b1b674872
Gia Khang <khanggia85@gmail.com>
2023-10-12 18:59:03 +0700

    linked to sheet

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import { Background } from "./Components/Background/Background";
 import { InputCode } from "./Components/Input/InputCode";
-import {Detail} from "./Components/Detail/Detail";
+import { Detail } from "./Components/Detail/Detail";
 import { ConfigProvider } from "antd";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+//Create new client
 
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false, // default: true
+    },
+  },
+});
 function App() {
   return (
-    <BrowserRouter>
-      <ConfigProvider theme={{ token: { colorPrimary: "#f56c64" } }}>
-        <Routes>
-          <Route path="/" element={<InputCode />} />
-          <Route path="/:code" element={<Detail />} />
-          <Route path="/*" element={<InputCode />} />
-        </Routes>
-        <Background />
-      </ConfigProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={client}>
+      <BrowserRouter>
+        <ConfigProvider theme={{ token: { colorPrimary: "#f56c64" } }}>
+          <Routes>
+            <Route path="/" element={<InputCode />} />
+            <Route path="/:code" element={<Detail />} />
+            <Route path="/*" element={<InputCode />} />
+          </Routes>
+          <Background />
+        </ConfigProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
--- src/Components/Detail/Detail.js
+++ src/Components/Detail/Detail.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./Detail.css";
-import { Row, Col, Typography, Tag, QRCode, Empty, Button } from "antd";
+import { Row, Col, Typography, Tag, QRCode, Empty, Button, Spin } from "antd";
 import { LeftOutlined } from "@ant-design/icons";
 import { useParams, useNavigate } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 export function Detail() {
   const { Title } = Typography;
@@ -10,76 +12,106 @@ export function Detail() {
 
   const navigate = useNavigate();
 
+  const {
+    data: user, //assign name for the data
+    isLoading,
+    // isError,
+    refetch,
+  } = useQuery(["u"], () => {
+    return axios
+      .get(`https://sheetdb.io/api/v1/5ns7w9461kjnd/search?mssv=${code}`)
+      .then((response) => response.data);
+  });
+
   return (
     <div className="DetailContainer">
-      <Button className="back" type="text" icon={<LeftOutlined />} onClick={() => navigate("/")}>
+      <Button
+        className="back"
+        type="text"
+        icon={<LeftOutlined />}
+        onClick={() => navigate("/")}
+      >
         Back
       </Button>
       <Title level={3}>INFORMATIONS</Title>
-      {code === "SE171927" ? (
-        <>
-          <Row className="rowDetail">
-            <Col xs={2}></Col>
-            <Col xs={5}>
-              <div className="detailTitle">TÊN:</div>
-            </Col>
-            <Col xs={17}>
-              <div className="detailInfo">Trương Nguyễn Gia Khang</div>
-            </Col>
-          </Row>
+      {!isLoading ? (
+        user.length > 0 && user[0]?.mssv === code ? (
+          <>
+            <Row className="rowDetail">
+              <Col xs={2}></Col>
+              <Col xs={5}>
+                <div className="detailTitle">TÊN:</div>
+              </Col>
+              <Col xs={17}>
+                <div className="detailInfo">{user[0]?.name}</div>
+              </Col>
+            </Row>
 
-          <Row className="rowDetail">
-            <Col xs={2}></Col>
-            <Col xs={5}>
-              <div className="detailTitle">MSSV:</div>
-            </Col>
-            <Col xs={17}>
-              <div className="detailInfo">SE171927</div>
-            </Col>
-          </Row>
+            <Row className="rowDetail">
+              <Col xs={2}></Col>
+              <Col xs={5}>
+                <div className="detailTitle">MSSV:</div>
+              </Col>
+              <Col xs={17}>
+                <div className="detailInfo">{user[0]?.mssv}</div>
+              </Col>
+            </Row>
 
-          <Row className="rowDetail">
-            <Col xs={2}></Col>
-            <Col xs={5}>
-              <div className="detailTitle">CODE:</div>
-            </Col>
-            <Col xs={17}>
-              <div className="detailInfo">HLW231234</div>
-            </Col>
-          </Row>
+            <Row className="rowDetail">
+              <Col xs={2}></Col>
+              <Col xs={5}>
+                <div className="detailTitle">CODE:</div>
+              </Col>
+              <Col xs={17}>
+                <div className="detailInfo">{user[0]?.code}</div>
+              </Col>
+            </Row>
 
-          <Row className="rowDetail">
-            <Col xs={2}></Col>
-            <Col xs={4}>
-              <Tag color="green">Trạm 1</Tag>
-            </Col>
-            <Col xs={4}>
-              <Tag color="red">Trạm 2</Tag>
-            </Col>
-            <Col xs={4}>
-              <Tag color="red">Trạm 3</Tag>
-            </Col>
-            <Col xs={4}>
-              <Tag color="red">Trạm 4</Tag>
-            </Col>
-            <Col xs={4}>
-              <Tag color="red">Trạm 5</Tag>
-            </Col>
-            <Col xs={2}></Col>
-          </Row>
+            <Row className="rowDetail">
+              <Col xs={2}></Col>
+              <Col xs={4}>
+                <Tag color={user[0]?.tram1 === "TRUE" ? "green" : "red"}>
+                  Trạm 1
+                </Tag>
+              </Col>
+              <Col xs={4}>
+                <Tag color={user[0]?.tram2 === "TRUE" ? "green" : "red"}>
+                  Trạm 2
+                </Tag>
+              </Col>
+              <Col xs={4}>
+                <Tag color={user[0]?.tram3 === "TRUE" ? "green" : "red"}>
+                  Trạm 3
+                </Tag>
+              </Col>
+              <Col xs={4}>
+                <Tag color={user[0]?.tram4 === "TRUE" ? "green" : "red"}>
+                  Trạm 4
+                </Tag>
+              </Col>
+              <Col xs={4}>
+                <Tag color={user[0]?.tram5 === "TRUE" ? "green" : "red"}>
+                  Trạm 5
+                </Tag>
+              </Col>
+              <Col xs={2}></Col>
+            </Row>
 
-          <Row style={{ margin: "20px 0 0 0" }}>
-            <Col xs={6}></Col>
-            <Col xs={10}>
-              <QRCode size={180} value={code}></QRCode>
-            </Col>
-            <Col xs={6}></Col>
-          </Row>
-        </>
+            <Row style={{ margin: "20px 0 0 0" }}>
+              <Col xs={6}></Col>
+              <Col xs={10}>
+                <QRCode size={180} value={user[0]?.code}></QRCode>
+              </Col>
+              <Col xs={6}></Col>
+            </Row>
+          </>
+        ) : (
+          <Empty description={`MSSV ${code} chưa đăng ký tham gia`}>
+            <Button type="primary">ĐĂNG KÝ</Button>
+          </Empty>
+        )
       ) : (
-        <Empty description={`MSSV ${code} chưa đăng ký tham gia`} > 
-           <Button type="primary">ĐĂNG KÝ</Button>
-        </Empty>
+        <Spin size="large" />
       )}
     </div>
   );
