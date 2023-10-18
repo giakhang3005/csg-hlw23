cbfeb0eec117e7cca4f50178a735238a58186a98
Gia Khang <khanggia85@gmail.com>
2023-10-18 17:22:18 +0700

    add logo

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,8 +1,9 @@
 import React from "react";
 import "./Nav.css";
-import { Button } from "antd";
+import { Button, Typography } from "antd";
 
 export function Nav() {
+  const { Title } = Typography;
   return (
     <>
       <img className="fpt" src="./assets/logofpt.png" />
@@ -10,6 +11,36 @@ export function Nav() {
         <img className="logo" src="./assets/logowhite.png" />
       </a>
 
+      <div className="LogoNTTContainer">
+        <div className="NTT">
+          <div className="titleNTT">NHÀ TÀI TRỢ BẠC</div>
+          <div className="cont">
+            <img className="logoNTT" src="./assets/NTT/Logo_Chill-01.png" />
+          </div>
+        </div>
+
+        <div className="NTT">
+          <div className="titleNTT">NHÀ TÀI TRỢ ĐỒNG</div>
+          <div className="cont">
+            <img className="logoNTT" src="./assets/NTT/LOGO HANOK.png" />
+            <img className="logoNTT" src="./assets/NTT/LOGO-HAPPY-THAI-1.png" />
+          </div>
+        </div>
+
+        <div className="NTT">
+          <div className="titleNTT">NHÀ TÀI TRỢ ĐỒNG HÀNH</div>
+          <div className="cont">
+            <img className="logoNTT" src="./assets/NTT/LOGO MRBROWN.png" />
+          </div>
+        </div>
+
+        <div className="DVDH">
+          <div className="titleNTT">ĐƠN VỊ ĐỒNG HÀNH</div>
+          <div className="cont">
+            <img className="logoDVDH" src="./assets/DVDH/JSC.png" />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
